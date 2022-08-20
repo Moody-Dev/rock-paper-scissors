d827f07bb5f5d2d2682bd825428c1aaaab548b83
Moody-Dev <huzaifaamin23@gmail.com>
2022-08-21 01:54:37 +0500

    Adding buttons

--- main.js
+++ main.js
@@ -1,78 +1,115 @@
-const startGame = document.querySelector('#container');
-startGame.addEventListener('click', playRound)
+const startGame = document.querySelector('#subBtn');
+startGame.addEventListener('click', (e) => {
+  e.target.setAttribute('id', 'hidden');
+  //const subBtn = document.querySelector('#subBtn');
+  //subBtn.setAttribute('id', 'hidden');
 
-
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissor = document.querySelector('#scissor');
-// const playerScore = document.querySelector('#playerScore');
-// const computerScore = document.querySelector('#computerScore');
-const computerChoice = document.querySelector('#computerChoice');
-
-
-
-const result = document.querySelector('#result');
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-  // Get Player Choice
-  button.addEventListener('click', () => {
-    const playerValue = button.getAttribute('name');
-    const playerChoice = document.createElement('div');
-    // playerChoice.textContent = `You Choose The: ${playerValue}`;
-    // result.appendChild(playerChoice);
-
-    const playerSelection = playerValue;
-    return playerSelection;
-  });
-
-  // Get Computer Choice
-  button.addEventListener('click', () => {
-    const gameChoice = ['rock', 'paper', 'scissor'];
-    const randomChoice = gameChoice[Math.floor(Math.random() * gameChoice.length)];
-    const computerSelection = randomChoice;
-    return computerSelection;
-
-    // return randomChoice;
-  });
-
-  function playRound(playerSelection, computerSelection) {
-    // answers.setAttribute('id', 'answers');
-    // answers.textContent = [];
-    switch (true) {
-      case (playerSelection === computerSelection):
-        // const answers = document.createElement('div');
-        const p = document.createElement('p');
-        p.textContent = 'Game Tie, Plese try again.';
-        answers.appendChild('p')
-        break;
-      case (playerSelection === 'rock' && computerSelection === 'scissor'):
-        let greetingRock = `You Choose: ${playerSelection} and`;
-        console.log(`${greetingRock} You Win, Rock beats the scissor`);
-        break;
-      case (playerSelection === 'rock' && computerSelection === 'paper'):
-        let courageRock = `You Choose: ${playerSelection} and`;
-        console.log(`${courageRock} You Lose, Paper beats the Rock`);
-        break;
-      case (playerSelection === 'scissor' && computerSelection === 'rock'):
-        let courageScissor = `You Choose: ${playerSelection} and`;
-        console.log(`${courageScissor} You Lose, Rock beats the scissor`);
-        break;
-      case (playerSelection === 'scissor' && computerSelection === 'paper'):
-        let greetingScissor = `You Choose: ${playerSelection} and`;
-        console.log(`${greetingScissor} You Win, Scissor beats the Paper`);
-        break;
-      case (playerSelection === 'paper' && computerSelection === 'scissors'):
-        let couragePaper = `You Choose: ${playerSelection} and`;
-        console.log(`${couragePaper} You Lose, Scissor beats the Paper`);
-        break;
-      case (playerSelection === 'paper' && computerSelection === 'rock'):
-        let greetingPaper = `You Choose: ${playerSelection} and`;
-        console.log(`${greetingPaper} You Win, Paper beats the Rock`);
-        break;
-    }
+  //Create Buttons
+  const buttons = ['Rock', 'Paper', 'Scissor']
+  const container = document.querySelector('#container');
+  const btnContainer = document.createElement('div');
+  for (const button of buttons) {
+    const createBtn = document.createElement('button');
+    createBtn.textContent = button;
+    createBtn.setAttribute('class', 'buttons');
+    btnContainer.setAttribute('class', 'btnContainer');
+    btnContainer.append(createBtn);
+    container.appendChild(btnContainer);
   }
-  playRound();
-});
+  //const container = document.querySelector('#container');
+  //const btnDiv = document.createElement('div');
+  //btnDiv.setAttribute('class', 'buttons')
+  //const btnRock = document.createElement('button');
+  //btnRock.setAttribute('type', 'submit');
+  //btnRock.textContent = 'Rock';
+  //const btnPaper= document.createElement('button');
+  //btnPaper.setAttribute('type', 'submit');
+  //btnPaper.textContent = 'Paper';
+  //const btnScissor= document.createElement('button');
+  //btnScissor.setAttribute('type', 'submit');
+  //btnScissor.textContent = 'Scissor';
+  //btnDiv.append(btnRock, btnPaper, btnScissor);
+  //container.appendChild(btnDiv);
+
+})
+
+
+//const rock = document.querySelector('#rock');
+//const paper = document.querySelector('#paper');
+//const scissor = document.querySelector('#scissor');
+//// const playerScore = document.querySelector('#playerScore');
+//// const computerScore = document.querySelector('#computerScore');
+//const computerChoice = document.querySelector('#computerChoice');
+
+
+
+//const result = document.querySelector('#result');
+//const buttons = document.querySelectorAll('button');
+//buttons.forEach((button) => {
+//  // Get Player Choice
+//  button.addEventListener('click', () => {
+//    const playerValue = button.getAttribute('name');
+//    const playerChoice = document.createElement('div');
+//    // playerChoice.textContent = `You Choose The: ${playerValue}`;
+//    // result.appendChild(playerChoice);
+
+//    const playerSelection = playerValue;
+//    return playerSelection;
+//  });
+
+//  // Get Computer Choice
+//  button.addEventListener('click', () => {
+//    const gameChoice = ['rock', 'paper', 'scissor'];
+//    const randomChoice = gameChoice[Math.floor(Math.random() * gameChoice.length)];
+//    const computerSelection = randomChoice;
+//    return computerSelection;
+
+//    // return randomChoice;
+//  });
+
+//  function playRound(playerSelection, computerSelection) {
+//    // answers.setAttribute('id', 'answers');
+//    // answers.textContent = [];
+//    switch (true) {
+//      case (playerSelection === computerSelection):
+//        // const answers = document.createElement('div');
+//        const p = document.createElement('p');
+//        p.textContent = 'Game Tie, Plese try again.';
+//        answers.appendChild('p')
+//        break;
+//      case (playerSelection === 'rock' && computerSelection === 'scissor'):
+//        let greetingRock = `You Choose: ${playerSelection} and`;
+//        console.log(`${greetingRock} You Win, Rock beats the scissor`);
+//        break;
+//      case (playerSelection === 'rock' && computerSelection === 'paper'):
+//        let courageRock = `You Choose: ${playerSelection} and`;
+//        console.log(`${courageRock} You Lose, Paper beats the Rock`);
+//        break;
+//      case (playerSelection === 'scissor' && computerSelection === 'rock'):
+//        let courageScissor = `You Choose: ${playerSelection} and`;
+//        console.log(`${courageScissor} You Lose, Rock beats the scissor`);
+//        break;
+//      case (playerSelection === 'scissor' && computerSelection === 'paper'):
+//        let greetingScissor = `You Choose: ${playerSelection} and`;
+//        console.log(`${greetingScissor} You Win, Scissor beats the Paper`);
+//        break;
+//      case (playerSelection === 'paper' && computerSelection === 'scissors'):
+//        let couragePaper = `You Choose: ${playerSelection} and`;
+//        console.log(`${couragePaper} You Lose, Scissor beats the Paper`);
+//        break;
+//      case (playerSelection === 'paper' && computerSelection === 'rock'):
+//        let greetingPaper = `You Choose: ${playerSelection} and`;
+//        console.log(`${greetingPaper} You Win, Paper beats the Rock`);
+//        break;
+//    }
+//  }
+//  playRound();
+//});
+
+
+
+
+
 // function playRound(playerSelection, computerSelection) {
 //     switch (true) {
 //         case (playerSelection === computerSelection):
