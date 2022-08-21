0f1d990ae881141cc592969c3c81a27dec428c30
Moody-Dev <huzaifaamin23@gmail.com>
2022-08-21 17:15:28 +0500

    Add Comparison and Answers.

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ startGame.addEventListener('click', (e) => {
   //const subBtn = document.querySelector('#subBtn');
   //subBtn.setAttribute('id', 'hidden');
 
-  //Create Buttons
+  //Create & Get Buttons
   const buttons = ['Rock', 'Paper', 'Scissor']
   const container = document.querySelector('#container');
   const btnContainer = document.createElement('div');
+  const answersContainer = document.createElement('div');
   for (const button of buttons) {
     const createBtn = document.createElement('button');
     createBtn.textContent = button;
@@ -19,81 +20,46 @@ startGame.addEventListener('click', (e) => {
     //Get Value form Player and Computer
     createBtn.addEventListener('click', (e) => {
       const playerSelection = e.target.getAttribute('name');
-      const computerSelection = () => {
-        for (const value of buttons) {
-          console.log(value);
-        }
-      }
-      console.log(computerSelection);
-      console.log(playerSelection);
-
+      const buttons = ['Rock', 'Paper', 'Scissor']
+      const computerSelection = buttons[Math.floor(Math.random() * buttons.length)];
 
       //Comparison for both Values
-      const answersContainer = document.createElement('div');
       const answer = document.createElement('p');
+      const gameTie = `Game Tie: Your Selection: ${playerSelection}, Computer Selection: ${computerSelection}`
+      const youWin = `You Win: Your Selection: ${playerSelection}, Computer Selection: ${computerSelection}`;
+      const youLose = `You Lose: Your Selection: ${playerSelection}, Computer Selection: ${computerSelection}`;
       answer.setAttribute('id', 'answer')
-      answer.textContent = [`You Selection: ${playerSelection}, Computer Selection: ${computerSelection}`];
-      answersContainer.appendChild(answer);
-      container.appendChild(answersContainer);
+
       switch (true) {
         case (playerSelection === computerSelection):
-          //answer.textContent = ['']
-          const tie = 'Game Tie, try again.'
+          answer.textContent = gameTie;
+          break;
+        case (playerSelection === 'Rock' && computerSelection === 'Scissor'):
+          answer.textContent = youWin;
+          break;
+        case (playerSelection === 'Rock' && computerSelection === 'Paper'):
+          answer.textContent = youLose;
+          break;
+        case (playerSelection === 'Scissor' && computerSelection === 'Paper'):
+          answer.textContent = youWin;
+          break;
+        case (playerSelection === 'Scissor' && computerSelection === 'Rock'):
+          answer.textContent = youLose;
+          break;
+        case (playerSelection === 'Paper' && computerSelection === 'Rock'):
+          answer.textContent = youWin;
+          break;
+        case (playerSelection === 'Paper' && computerSelection === 'Scissor'):
+          answer.textContent = youLose;
+          break;
       }
+
+      //Set Comparison Answer
+      answersContainer.appendChild(answer);
+      container.appendChild(answersContainer);
+
+
     })
   }
 
 })
-
-
-
-// function playRound(playerSelection, computerSelection) {
-//     switch (true) {
-//         case (playerSelection === computerSelection):
-//             console.log('Game Tie, please try again.');
-//             break;
-//         case (playerSelection === 'rock' && computerSelection === 'scissor'):
-//             let greetingRock = `You Choose: ${playerSelection} and`;
-//             console.log(`${greetingRock} You Win, Rock beats the scissor`);
-//             break;
-//         case (playerSelection === 'rock' && computerSelection === 'paper'):
-//             let courageRock = `You Choose: ${playerSelection} and`;
-//             console.log(`${courageRock} You Lose, Paper beats the Rock`);
-//             break;
-//         case (playerSelection === 'scissor' && computerSelection === 'rock'):
-//             let courageScissor = `You Choose: ${playerSelection} and`;
-//             console.log(`${courageScissor} You Lose, Rock beats the scissor`);
-//             break;
-//         case (playerSelection === 'scissor' && computerSelection === 'paper'):
-//             let greetingScissor = `You Choose: ${playerSelection} and`;
-//             console.log(`${greetingScissor} You Win, Scissor beats the Paper`);
-//             break;
-//         case (playerSelection === 'paper' && computerSelection === 'scissors'):
-//             let couragePaper = `You Choose: ${playerSelection} and`;
-//             console.log(`${couragePaper} You Lose, Scissor beats the Paper`);
-//             break;
-//         case (playerSelection === 'paper' && computerSelection === 'rock'):
-//             let greetingPaper = `You Choose: ${playerSelection} and`;
-//             console.log(`${greetingPaper} You Win, Paper beats the Rock`);
-//             break;
-//     }
-// }
-
-// function getPlayerChoice() {
-//     const userValue = prompt('Choose your Item:', '').toLowerCase();
-//     if (userValue == null || userValue == '') {
-//         prompt('Please choose one of: Rock, Paper, Scissor');
-//     }
-//     const playerSelection = userValue;
-//     const computerSelection = getComputerChoice();
-//     console.log(playRound(playerSelection, computerSelection));
-// }
-
-// function game() {
-//     let playAgain;
-//     for (let i = 0; i < 5; i++) {
-//         playAgain = getPlayerChoice();
-//     }
-// }
-// game()
-
